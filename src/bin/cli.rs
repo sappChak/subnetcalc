626fb50f2a8cc80c18ee9f026f6153b2d913581c
Andrii Konotop <konotop401@gmail.com>
2024-10-09 22:00:36 +0200

    feat(cli): create new command for the mask feature

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,26 +1,40 @@
 use clap::{Parser, Subcommand};
-use colored::Colorize;
-use log::info;
+use colored::*;
 use std::str::FromStr;
-use subnetcalc::subnet::Subnet;
+use subnetcalc::subnet::Network;
 
 #[derive(Parser)]
-#[command(name = "subnetcalc")]
-#[command(about = "A tool for subnet calculations", long_about = None)]
+#[command(name = "subnetcalc", about = "A tool for subnet calculations")]
 pub struct Cli {
     #[command(subcommand)]
-    pub command: Command,
+    pub command: Commands,
 }
 
 #[derive(Subcommand)]
-pub enum Command {
+pub enum Commands {
+    /// Aggregate multiple networks into one larger network
     Aggregate {
+        /// List of networks to aggregate (in CIDR notation)
         #[arg(required = true)]
-        subnets: Vec<String>,
+        networks: Vec<String>,
     },
+    /// Display information about a specific network
     Info {
+        /// Network to display information for (in CIDR notation)
         #[arg(required = true)]
-        subnet: String,
+        network: String,
+    },
+    /// Calculate the mask for a given number of hosts and networks
+    Mask {
+        // Network to calculate the mask for
+        #[arg(required = true)]
+        network: String,
+        /// Number of required hosts
+        #[arg(required = true)]
+        hosts: u32,
+        /// Number of required networks
+        #[arg(required = true)]
+        networks_number: u32,
     },
 }
 
@@ -44,46 +58,96 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse_from(args);
 
     match &cli.command {
-        Command::Aggregate { subnets } => handle_aggregate(subnets),
-        Command::Info { subnet } => handle_info(subnet),
+        Commands::Aggregate { networks } => handle_aggregate(networks),
+        Commands::Info { network } => handle_info(network),
+        Commands::Mask {
+            network,
+            hosts,
+            networks_number: networks,
+        } => handle_mask(network, *hosts, *networks),
     }
 }
 
-fn handle_aggregate(subnets: &[String]) -> Result<(), Box<dyn std::error::Error>> {
-    let parsed_subnets = parse_subnets(subnets)?;
-    match Subnet::aggregate(&parsed_subnets) {
-        Ok(aggregated_subnet) => {
-            info!(
-                "Aggregated subnet: {}",
-                aggregated_subnet.to_string().purple()
+fn handle_aggregate(networks: &[String]) -> Result<(), Box<dyn std::error::Error>> {
+    let parsed_networks: Vec<Network> = parse_networks(networks)?;
+    match Network::aggregate_networks(&parsed_networks) {
+        Ok(aggregated_network) => {
+            println!(
+                "{}: {}",
+                "Aggregated Network".bold().green(),
+                aggregated_network.to_string().purple()
             );
         }
         Err(e) => {
-            println!("Error: {}", e.to_string().red());
+            println!("{}: {}", "Error".bold().red(), e.to_string().red());
         }
     }
     Ok(())
 }
 
-fn handle_info(subnet: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let subnet = Subnet::from_str(subnet)?;
-    display_subnet_info(&subnet);
+fn handle_info(network_str: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let network = Network::from_str(network_str)?;
+    display_network_info(&network);
     Ok(())
 }
 
-fn parse_subnets(subnets: &[String]) -> Result<Vec<Subnet>, Box<dyn std::error::Error>> {
-    subnets
-        .iter()
-        .map(|s| Subnet::from_str(s))
-        .collect::<Result<_, _>>()
-        .map_err(|e| e.into())
+fn handle_mask(
+    network: &str,
+    required_hosts: u32,
+    required_networks: u32,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let parsed_network = Network::from_str(network)?;
+    match Network::determine_subnet_mask(&parsed_network, required_hosts, required_networks) {
+        Ok(mask) => {
+            println!(
+                "{}: {}",
+                "Subnet Mask".bold().green(),
+                mask.to_string().yellow()
+            );
+        }
+        Err(e) => {
+            println!("{}: {}", "Error".bold().red(), e.red());
+        }
+    }
+    Ok(())
 }
 
-fn display_subnet_info(subnet: &Subnet) {
-    info!("Subnet: {}", subnet.to_string().purple());
-    info!("Netmask: {}", subnet.netmask().to_string().purple());
-    info!("Wildcard: {}", subnet.wildcard().to_string().purple());
-    info!("Broadcast: {}", subnet.broadcast().to_string().purple());
-    info!("Hosts: {}", subnet.hosts().to_string().purple());
-    info!("Class type: {}", subnet.class().to_string());
+fn parse_networks(networks: &[String]) -> Result<Vec<Network>, Box<dyn std::error::Error>> {
+    networks
+        .iter()
+        .map(|s| Network::from_str(s).map_err(|e| e.into()))
+        .collect::<Result<Vec<_>, _>>()
+}
+
+fn display_network_info(network: &Network) {
+    println!(
+        "{}: {}",
+        "Network".bold().green(),
+        network.to_string().purple()
+    );
+    println!(
+        "{}: {}",
+        "Netmask".bold().green(),
+        network.netmask_address().to_string().yellow()
+    );
+    println!(
+        "{}: {}",
+        "Wildcard".bold().green(),
+        network.wildcard_address().to_string().yellow()
+    );
+    println!(
+        "{}: {}",
+        "Broadcast".bold().green(),
+        network.broadcast_address().to_string().yellow()
+    );
+    println!(
+        "{}: {}",
+        "Available Hosts".bold().green(),
+        network.available_hosts().to_string().yellow()
+    );
+    println!(
+        "{}: {}",
+        "Class".bold().green(),
+        network.ip_class().to_string().cyan()
+    );
 }
